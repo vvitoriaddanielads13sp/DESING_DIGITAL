c72986a005e6a585946cf4ffd5cb2f8a839d28ce
vvitoriaddanielads13sp <vitoriadaniela97@gmail.com>
2019-12-02 22:12:24 -0200

    jquery

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    $('.BIGS').hide()});
+    $('#mini1').click(function(){
+    $('#DIVMOSTRAR').html('<img src="mini_BW/aa2.jpg">')
+    });
+    
+    $('#mini2').click(function(){
+    $('#DIVMOSTRAR').html('<img src="mini_BW/bb1.jpg">')
+    });
 
-event.preventDefault();
+    $('#mini3').click(function(){
+    $('#DIVMOSTRAR').html('<img src="mini_BW/cc1.jpg">')
+    });
 
+    $('#mini4').click(function(){
+    $('#DIVMOSTRAR').html('<img src="mini_BW/aa.jpeg">')
+    });
+
+    $('#mini5').click(function(){
+    $('#DIVMOSTRAR').html('<img src="mini_BW/BB.jpeg">')
+    });
+
+    $('#mini6').click(function(){
+    $('#DIVMOSTRAR').html('<img src="mini_BW/mini5.jpeg">')
+    });
+});
